18565dc10ce9eabd99a8b61c655418ea982fda4a
Emmanuel Anebi <anebiemmanuel@gmail.com>
2025-05-03 14:33:35 +0100

    feat: update deploy workflow to include API_BASE_URL environment variable

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-import { env } from "@/env";
-
 import AuthUIWrapper from "@/components/auth/AuthUIWrapper";
 import PasswordSignIn from "@/components/auth/PasswordSignIn";
 
 function LoginPage() {
   return (
     <AuthUIWrapper mode="email_signin">
-      <span className="text-red-500">{env.NEXT_PUBLIC_API_BASE_URL}</span>
       <PasswordSignIn />
     </AuthUIWrapper>
   );
