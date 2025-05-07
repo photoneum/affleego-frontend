679ecbe41c9f133fb8e013f2d5b375e28917eb6a
Emmanuel Anebi <anebiemmanuel@gmail.com>
2025-05-07 12:25:53 +0100

    feat: wrap VerifyUserEmail component in Suspense for improved loading state management

--- app/auth/verify-account/page.tsx
+++ app/auth/verify-account/page.tsx
@@ -1,14 +1,16 @@
 // http://localhost:3000/auth/verify-account/?email=polyf@mailinator.com&code=281813
-import React from "react";
+import React, { Suspense } from "react";
 
 import AuthUIWrapper from "@/components/auth/AuthUIWrapper";
 import VerifyUserEmail from "@/components/auth/VerifyUserEmail";
 
 function VerifyAccount() {
   return (
-    <AuthUIWrapper mode="verify_account">
-      <VerifyUserEmail />
-    </AuthUIWrapper>
+    <Suspense fallback={<div>Loading...</div>}>
+      <AuthUIWrapper mode="verify_account">
+        <VerifyUserEmail />
+      </AuthUIWrapper>
+    </Suspense>
   );
 }
 
