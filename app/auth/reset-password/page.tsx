b67beb3814d5081aae57c3a87ccfd62e12695271
Emmanuel Anebi <anebiemmanuel@gmail.com>
2025-05-11 11:17:33 +0100

    refactor: simplify PasswordResetWithTokenPage by removing token parameter from PasswordReset

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -3,15 +3,10 @@ import React from "react";
 import AuthUIWrapper from "@/components/auth/AuthUIWrapper";
 import PasswordReset from "@/components/auth/PasswordReset";
 
-type Props = {
-  params: Promise<{ token: string }>;
-};
-
-async function PasswordResetWithTokenPage({ params }: Props) {
-  const { token } = await params;
+async function PasswordResetWithTokenPage() {
   return (
     <AuthUIWrapper mode="reset_password">
-      <PasswordReset token={token} />
+      <PasswordReset />
     </AuthUIWrapper>
   );
 }
